a2424477a28c7d2e63deaacf5077353f2bbc9326
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-11-12 11:28:59 +0300

    add activation error

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('mainclone.urls')),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
-    url(r'^', include('mainclone.urls'))
 ]
