418258f537fb2c180b4c082b7d9a9c8832f20af2
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-11-12 14:52:10 +0300

    deploy

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -111,17 +111,17 @@ WSGI_APPLICATION = 'instaclone.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': os.getenv('DB_PORT', '5432'),
-            }
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': config('DB_NAME'),
+       'USER': config('DB_USER'),
+       'PASSWORD': config('DB_PASSWORD'),
+       'HOST': config('DB_HOST'),
+       'PORT': os.getenv('DB_PORT', '5432'),
+        }
 
-   }
+}
 # production
 
 
